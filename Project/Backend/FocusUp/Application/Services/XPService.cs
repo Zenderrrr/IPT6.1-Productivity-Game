70cf2fc7d17a9caa05c96e03e1f4ca490c11fa66
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-05 23:03:23 +0200

    Update XPService add namespace & implemented all methods

--- Project/Backend/FocusUp/Application/Services/XPService.cs
+++ Project/Backend/FocusUp/Application/Services/XPService.cs
@@ -1,36 +1,50 @@
 ﻿using FocusUp.Application.Strategies.Interfaces;
+using FocusUp.Domain.Enums;
 using System;
 
-public class XPService
+namespace FocusUp.Application.Services
 {
-    private readonly XPEventRepository _eventRepository;
-    private readonly UserStatsRepository _userStatsRepository;
-    private readonly IXpCalculationStrategy _xpStrategy;
-
-    public XPService(XPEventRepository xPEventRepository, UserStatsRepository userStatsRepository, IXpCalculationStrategy xpStrategy )
+    public class XPService
     {
-        _eventRepository = xPEventRepository;
-        _userStatsRepository = userStatsRepository;
-        _xpStrategy = xpStrategy;
-    }
+        private readonly XPEventRepository _xPEventRepository;
+        private readonly UserStatsRepository _userStatsRepository;
+        private readonly IXpCalculationStrategy _xpStrategy;
 
-    public int CalculateXP(Task task, int streakCount)
-    {
-        throw new NotImplementedException();
-    }
+        public XPService(XPEventRepository xPEventRepository, UserStatsRepository userStatsRepository, IXpCalculationStrategy xpStrategy)
+        {
+            _xPEventRepository = xPEventRepository;
+            _userStatsRepository = userStatsRepository;
+            _xpStrategy = xpStrategy;
+        }
 
-    public int AwardXP(int userId, Task task, int streakCount, string reason)
-    {
-        throw new NotImplementedException();
-    }
+        public int CalculateXP(Task task, int streakCount)
+        {
+            return _xpStrategy.CalculateXP(task, streakCount);
+        }
 
-    public int GetTotalXP(int userId)
-    {
-        throw new NotImplementedException();
-    }
+        public int AwardXP(int userId, Task task, int streakCount, RewardReason reason)
+        {
+            int awardedXp = CalculateXP(task, streakCount);
+            XpEvent xpEvent = new XpEvent(userId, awardedXp, reason, task.Id);
+            _xPEventRepository.Insert(xpEvent);
 
-    public bool HasXPEventForTask(int taskId, string reason)
-    {
-        throw new NotImplementedException();
+            UserStats? userStats = _userStatsRepository.GetByUserId(userId) ?? throw new UserStatsNotFoundException(userId);
+            userStats.AddXp(awardedXp);
+            _userStatsRepository.UpdateTotalXp(userId, userStats.TotalXp);
+
+            return awardedXp;
+        }
+
+        public int GetTotalXP(int userId)
+        {
+            UserStats? userStats = _userStatsRepository.GetByUserId(userId) ?? throw new UserStatsNotFoundException(userId);
+            return userStats.TotalXp;
+        }
+
+        public bool HasXPEventForTask(int taskId, RewardReason reason)
+        {
+            var xpEvent = _xPEventRepository.GetAllByTaskId(taskId);
+            return xpEvent.Any(e => e.Reason == reason);
+        }
     }
 }
