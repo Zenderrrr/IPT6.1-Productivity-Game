87199255cf8a3b530c0d587ff655fc7d11c9dfd1
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-05 23:01:05 +0200

    Update BadgeService - implemented all methods

--- Project/Backend/FocusUp/Application/Services/BadgeService.cs
+++ Project/Backend/FocusUp/Application/Services/BadgeService.cs
@@ -1,4 +1,6 @@
 ﻿using FocusUp.Application.Strategies.Interfaces;
+using FocusUp.Common.Exceptions;
+using FocusUp.Domain.Enums;
 using System;
 
 namespace FocusUp.Application.Services
@@ -20,22 +22,56 @@ namespace FocusUp.Application.Services
 
         public List<Badge> CheckAndAwardBadges(int userId)
         {
-            throw new NotImplementedException();
+            DateTime awardedAt = DateTime.Now;
+
+            var badges = GetAvailableBadges();
+            UserStats? userStats = _userStatsRepository.GetByUserId(userId) ?? throw new UserStatsNotFoundException(userId);
+
+            List<Badge> awardedBadges = new();
+
+            var userBadges = _userBadgeRepository.GetByUserId(userId);
+            foreach (var badge in badges)
+            {
+                BadgeRuleType badgeRuleType = badge.RuleType;
+
+                bool hasBadge = HasBadge(userBadges, badge.Id);
+                if (hasBadge) continue;
+
+                var rule = _badgeRules.FirstOrDefault(b => b.GetRuleType() == badgeRuleType) ?? throw new BadgeRuleNotFoundException(badgeRuleType, badge.Id);
+                bool isUnlocked = rule.IsUnlocked(userStats, badge);
+                if (!isUnlocked) continue;
+
+                UserBadge userBadge = new UserBadge(userId, badge.Id, awardedAt);
+                _userBadgeRepository.Insert(userBadge);
+
+                awardedBadges.Add(badge);
+            }
+            return awardedBadges;
         }
+
+        public bool HasBadge(List<UserBadge> userBadges, int badgeId) => userBadges.Any(u => u.BadgeId == badgeId);
 
         public bool HasBadge(int userId, int badgeId)
         {
-            throw new NotImplementedException();
+            var userBadges = _userBadgeRepository.GetByUserId(userId);
+            return userBadges.Any(u => u.BadgeId == badgeId);
         }
 
         public List<Badge> GetUnlockedBadges(int userId)
         {
-            throw new NotImplementedException();
+            var badges = GetAvailableBadges();
+            List<Badge> awardedBadges = new();
+
+            var userBadges = _userBadgeRepository.GetByUserId(userId);
+            foreach (var badge in badges)
+            {
+                bool hasBadge = HasBadge(userBadges, badge.Id);
+                if (!hasBadge) continue;
+                awardedBadges.Add(badge);
+            }
+            return awardedBadges;
         }
 
-        public List<Badge> GetAvailableBadges()
-        {
-            throw new NotImplementedException();
-        }
+        public List<Badge> GetAvailableBadges() => _badgeRepository.GetAll();
     }
 }
