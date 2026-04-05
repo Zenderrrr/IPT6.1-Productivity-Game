0c96a91746f9fedc74ef3717ab57f2e3f6b0268d
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-05 23:04:51 +0200

    Create RewardReson enum

---
+++ Project/Backend/FocusUp/Domain/Enums/RewardReason.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace FocusUp.Domain.Enums
+{
+    public enum RewardReason
+    {
+        TaskCompleted,
+        StreakBonus,
+        LevelUp,
+        BadgeUnlocked,
+        ManualAdjustment
+    }
+}
