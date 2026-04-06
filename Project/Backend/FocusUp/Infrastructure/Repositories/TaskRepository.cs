ada320a81ea09729567adad4b0d8c90612ae504e
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-06 18:19:11 +0200

    Update TaskRepository implemented all methods

--- Project/Backend/FocusUp/Infrastructure/Repositories/TaskRepository.cs
+++ Project/Backend/FocusUp/Infrastructure/Repositories/TaskRepository.cs
@@ -1,64 +1,220 @@
-using System;
 using FocusUp.Domain.Enums;
+using Microsoft.Data.Sqlite;
+using System;
+using static FocusUp.Domain.Enums.TaskStatus;
 
-public class TaskRepository : BaseRepository<Task>
+namespace FocusUp.Infrastructure.Repositories
 {
-    public TaskRepository(DatabaseConnection databaseConnection) : base(databaseConnection, "Task")
+    public class TaskRepository : BaseRepository<Task>
     {
-    }
+        public TaskRepository(DatabaseConnection databaseConnection) : base(databaseConnection, "Task")
+        {
+        }
 
-    public override Task? GetById(int id)
-    {
-        return base.GetById(id);
-    }
+        public override Task? GetById(int id)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
 
-    public List<Task> GetAllByUserId(int userId)
-    {
-        throw new NotImplementedException();
-    }
+            cmd.CommandText = $@"SELECT * FROM {_tableName}
+                                WHERE id = @id";
 
-    public List<Task> GetOpenByUserId(int userId)
-    {
-        throw new NotImplementedException();
-    }
+            cmd.Parameters.AddWithValue("@id", id);
 
-    public List<Task> GetCompletedByUserId(int userId)
-    {
-        throw new NotImplementedException();
-    }
+            using var reader = cmd.ExecuteReader();
 
-    public override int Insert(Task entity)
-    {
-        return base.Insert(entity);
-    }
+            if (!reader.Read())
+                return null;
 
-    public override void Update(Task entity)
-    {
-        base.Update(entity);
-    }
+            return MapToTask(reader);
+        }
 
-    public override void Delete(int id)
-    {
-        base.Delete(id);
-    }
+        public List<Task> GetAllByUserId(int userId)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
 
-    public bool Exists(int id)
-    {
-        throw new NotImplementedException();
-    }
+            cmd.CommandText = $@"SELECT * FROM {_tableName}
+                                WHERE user_id = @user_id";
+            cmd.Parameters.AddWithValue("@user_id", userId);
 
-    public void UpdateStatus(int taskId, FocusUp.Domain.Enums.TaskStatus status, DateTime? completedAt)
-    {
-        throw new NotImplementedException();
-    }
+            using var reader = cmd.ExecuteReader();
 
-    public int CountOpenTasks(int userId)
-    {
-        throw new NotImplementedException();
-    }
+            var tasks = new List<Task>();
+            while (reader.Read())
+                tasks.Add(MapToTask(reader));
 
-    public int CountCompletedTasks()
-    {
-        throw new NotImplementedException();
+            return tasks;
+        }
+
+        public List<Task> GetOpenByUserId(int userId)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = $@"SELECT * FROM {_tableName}
+                                 WHERE (status = '{Open.ToString()}' OR status = '{InProgress.ToString()}')  AND user_id = @user_id";
+            cmd.Parameters.AddWithValue("@user_id", userId);
+
+            using var reader = cmd.ExecuteReader();
+
+            var tasks = new List<Task>();
+            while (reader.Read())
+                tasks.Add(MapToTask(reader));
+            return tasks;
+        }
+
+        public List<Task> GetCompletedByUserId(int userId)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = $@"SELECT * FROM {_tableName}
+                                 WHERE status = '{Completed.ToString()}' AND user_id = @user_id";
+            cmd.Parameters.AddWithValue("@user_id", userId);
+
+            using var reader = cmd.ExecuteReader();
+
+            var tasks = new List<Task>();
+            while (reader.Read())
+                tasks.Add(MapToTask(reader));
+            return tasks;
+        }
+
+        public override int Insert(Task task)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = $@"INSERT INTO {_tableName} 
+                                 (user_id, category_id, title, description, difficulty, duration_min, due_date, status)
+                                 VALUES (@user_id, @category_id, @title, @description, @difficulty, @duration_min, @due_date, @status);
+                                 SELECT last_insert_rowid()";
+            
+            cmd.Parameters.AddWithValue("@user_id", task.UserId);
+            cmd.Parameters.AddWithValue("@category_id", task.CategoryId);
+            cmd.Parameters.AddWithValue("@title", task.Title);
+            cmd.Parameters.AddWithValue("@description", task.Description);
+            cmd.Parameters.AddWithValue("@difficulty", task.Difficulty.ToString());
+            cmd.Parameters.AddWithValue("@duration_min", task.DurationMin);
+            cmd.Parameters.AddWithValue("@due_date", task.DueDate);
+            cmd.Parameters.AddWithValue("@status", task.Status.ToString());
+
+            var id = cmd.ExecuteScalar();
+
+            return Convert.ToInt32(id);
+        }
+
+        public override void Update(Task task)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = $@"UPDATE {_tableName}
+                                 SET category_id = @category_id, title = @title, description = @description, difficulty = @difficulty, duration_min = @duration_min, due_date = @due_date, status = @status, completed_at = @completed_at, updated_at = @updated_at
+                                 WHERE id = @id";
+            
+            cmd.Parameters.AddWithValue("@id", task.Id);
+            cmd.Parameters.AddWithValue("@category_id", task.CategoryId);
+            cmd.Parameters.AddWithValue("@title", task.Title);
+            cmd.Parameters.AddWithValue("@description", task.Description);
+            cmd.Parameters.AddWithValue("@difficulty", task.Difficulty.ToString());
+            cmd.Parameters.AddWithValue("@duration_min", task.DurationMin);
+            cmd.Parameters.AddWithValue("@due_date", task.DueDate);
+            cmd.Parameters.AddWithValue("@status", task.Status.ToString());
+            cmd.Parameters.AddWithValue("@completed_at", task.CompletedAt);
+            cmd.Parameters.AddWithValue("@updated_at", task.UpdatedAt);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public bool Exists(int id)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = $@"SELECT 1 FROM {_tableName} WHERE id = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+
+            using var reader = cmd.ExecuteReader();
+
+            if (!reader.Read())
+                return false;
+            return true;
+        }
+
+        public void UpdateStatus(int taskId, Domain.Enums.TaskStatus status, DateTime? completedAt)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = $@"UPDATE {_tableName}
+                                 SET status = @status, completed_at = @completed_at, updated_at = @updated_at
+                                 WHERE id = @id";
+
+            cmd.Parameters.AddWithValue("@id", taskId);
+            cmd.Parameters.AddWithValue("@status", status.ToString());
+            cmd.Parameters.AddWithValue("@completed_at", completedAt);
+            cmd.Parameters.AddWithValue("@updated_at", DateTime.Now);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public int CountOpenTasks(int userId)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = $@"SELECT COUNT(*) AS 'Count' FROM {_tableName}
+                                 WHERE user_id = @user_id AND (status = '{Open.ToString()}' OR status = '{InProgress.ToString()}')";
+            cmd.Parameters.AddWithValue("@user_id", userId);
+
+            var count = cmd.ExecuteScalar();
+
+            return Convert.ToInt32(count);
+        }
+
+        public int CountCompletedTasks(int userId)
+        {
+            var connection = _dbConnection.GetConnection();
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = $@"SELECT COUNT(*) AS 'Count' FROM {_tableName}
+                                 WHERE user_id = @user_id AND status = '{Completed.ToString()}'";
+            cmd.Parameters.AddWithValue("@user_id", userId);
+
+            var count = cmd.ExecuteScalar();
+
+            return Convert.ToInt32(count);
+        }
+
+        private static Task MapToTask(SqliteDataReader reader)
+        {
+            int categoryId = reader.GetOrdinal("category_id");
+            int dueDateId = reader.GetOrdinal("due_date");
+            int completedAtId = reader.GetOrdinal("completed_at");
+
+            int? categoryIndex = reader.IsDBNull(categoryId) ? null : reader.GetInt32(categoryId);
+            DateTime? dueDate = reader.IsDBNull(dueDateId) ? null : reader.GetDateTime(dueDateId);
+            DateTime? completedAt = reader.IsDBNull(completedAtId) ? null : reader.GetDateTime(completedAtId);
+
+            Task task = new Task(
+                    reader.GetInt32(reader.GetOrdinal("user_id")),
+                    reader.GetString(reader.GetOrdinal("title")),
+                    reader.GetString(reader.GetOrdinal("description")),
+                    Enum.Parse<TaskDifficultyType>(reader.GetString(reader.GetOrdinal("difficulty"))),
+                    reader.GetInt32(reader.GetOrdinal("duration_min")),
+                    Enum.Parse<Domain.Enums.TaskStatus>(reader.GetString(reader.GetOrdinal("status"))),
+                    categoryIndex,
+                    dueDate
+                );
+
+            task.SetId(reader.GetInt32(reader.GetOrdinal("id")));
+            task.SetCreatedAt(reader.GetDateTime(reader.GetOrdinal("created_at")));
+            task.SetUpdatedAt(reader.GetDateTime(reader.GetOrdinal("updated_at")));
+            task.SetCompletedAt(completedAt);
+
+            return task;
+        }
     }
 }
