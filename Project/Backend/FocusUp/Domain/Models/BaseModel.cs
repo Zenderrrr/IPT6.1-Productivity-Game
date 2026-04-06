be9670d3d2f4909bd1e6e30732f6de6b8ffb3da1
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-06 16:12:17 +0200

    Update BaseModel add SetID, SetCreatedAt, SetUpdatedAt
    remove CreatedAt DateTime and add protected set; instead of no write access

--- Project/Backend/FocusUp/Domain/Models/BaseModel.cs
+++ Project/Backend/FocusUp/Domain/Models/BaseModel.cs
@@ -3,8 +3,14 @@ using System;
 public abstract class BaseModel
 { 
     public int Id { get; protected set; }
-    public DateTime CreatedAt { get; } = DateTime.Now;
+    public DateTime CreatedAt { get; protected set; }
     public DateTime UpdatedAt { get; protected set; }
+
+    public void SetId(int id) => Id = id;
+
+    public void SetCreatedAt(DateTime createdAt) =>  CreatedAt = createdAt;
+
+    public void SetUpdatedAt(DateTime updatedAt) => UpdatedAt = updatedAt;
 
     public virtual bool ValidateData()
     {
