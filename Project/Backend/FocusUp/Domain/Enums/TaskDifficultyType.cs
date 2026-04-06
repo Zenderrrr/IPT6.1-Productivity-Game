b1ee886e908f0c0b84876913adcf8ca32997adab
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-06 16:09:33 +0200

    Create TaskDifficultyType.cs

---
+++ Project/Backend/FocusUp/Domain/Enums/TaskDifficultyType.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FocusUp.Domain.Enums
+{
+    public enum TaskDifficultyType
+    {
+        easy = 1,
+        middle = 2,
+        hard = 3,
+        veryHard = 4,
+    }
+}
