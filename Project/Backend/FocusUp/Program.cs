b9a9c7a36ab2765f6f216c5a6bbdbf28ef5cae7c
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-07 18:31:12 +0200

    Update Program.cs prepare for endpoint implementation

--- Project/Backend/FocusUp/Program.cs
+++ Project/Backend/FocusUp/Program.cs
@@ -1,10 +1,48 @@
+using FocusUp.Application.Services;
+using FocusUp.Application.Strategies;
+using FocusUp.Application.Strategies.Interfaces;
+using FocusUp.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddSingleton(DatabaseConnection.GetInstance(connectionString));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<BadgeService>();
+builder.Services.AddScoped<LevelService>();
+builder.Services.AddScoped<StreakService>();
+builder.Services.AddScoped<TaskCompletionService>();
+builder.Services.AddScoped<TaskService>();
+builder.Services.AddScoped<XPService>();
+
+builder.Services.AddScoped<IStreakRuleStrategy, DefaultStreakRuleStrategy>();
+builder.Services.AddScoped<IXpCalculationStrategy, DefaultXpCalculationStrategy>();
+builder.Services.AddScoped<ILevelStrategy, QuadraticLevelStrategy>();
+builder.Services.AddScoped<IBadgeRule, StreakBadgeRule>();
+
+builder.Services.AddScoped<BadgeRepository>();
+builder.Services.AddScoped<TaskLogRepository>();
+builder.Services.AddScoped<TaskRepository>();
+builder.Services.AddScoped<UserBadgeRepository>();
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<UserStatsRepository>();
+builder.Services.AddScoped<XPEventRepository>();
+
 var app = builder.Build();
+
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -13,29 +51,4 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
