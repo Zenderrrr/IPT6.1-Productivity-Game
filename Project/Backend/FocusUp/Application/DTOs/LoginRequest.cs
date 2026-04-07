9a6c025f1d6f0fd5ad08fe43bb7830e0c8ce2dca
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-08 00:37:29 +0200

    Add request class for register and login

---
+++ Project/Backend/FocusUp/Application/DTOs/LoginRequest.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace FocusUp.Application.DTOs
+{
+    public class LoginRequest
+    {
+        public string UsernameOrEmail {  get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
