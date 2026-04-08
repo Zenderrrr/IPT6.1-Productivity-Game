a00c65050e86ceb915f8daf152a1db64fb651dca
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-09 01:27:34 +0200

    Create CategoryRequest.cs

---
+++ Project/Backend/FocusUp/Application/DTOs/CategoryRequest.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace FocusUp.Application.DTOs
+{
+    public class CategoryRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+    }
+}
