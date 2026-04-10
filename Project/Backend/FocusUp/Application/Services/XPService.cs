e4969c996b3464e6552212b25525d43fd3a33858
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-11 01:04:01 +0200

    Update XpService
    change responsibilities for AwardXp (int streakCount to int awardXp)
    
    add method AwardXP for transaction

--- Project/Backend/FocusUp/Application/Services/XPService.cs
+++ Project/Backend/FocusUp/Application/Services/XPService.cs
@@ -3,6 +3,7 @@ using FocusUp.Domain.Enums;
 using FocusUp.Infrastructure.Repositories;
 using System;
 using FocusUp.Common.Exceptions;
+using Microsoft.Data.Sqlite;
 
 namespace FocusUp.Application.Services
 {
@@ -24,17 +25,28 @@ namespace FocusUp.Application.Services
             return _xpStrategy.CalculateXP(task, streakCount);
         }
 
-        public int AwardXP(int userId, Task task, int streakCount, RewardReason reason)
+        public int AwardXP(int userId, Task task, int awardXp, RewardReason reason)
         {
-            int awardedXp = CalculateXP(task, streakCount);
-            XpEvent xpEvent = new XpEvent(userId, awardedXp, reason, task.Id);
+            XpEvent xpEvent = new XpEvent(userId, awardXp, reason, task.Id);
             _xPEventRepository.Insert(xpEvent);
 
             UserStats? userStats = _userStatsRepository.GetByUserId(userId) ?? throw new UserStatsNotFoundException(userId);
-            userStats.AddXp(awardedXp);
+            userStats.AddXp(awardXp);
             _userStatsRepository.UpdateTotalXp(userId, userStats.TotalXp);
 
-            return awardedXp;
+            return awardXp;
+        }
+
+        public int AwardXP(Task task, int awardXp, RewardReason reason, SqliteConnection connection, SqliteTransaction transaction)
+        {
+            XpEvent xpEvent = new(task.UserId, awardXp, reason, task.Id);
+            _xPEventRepository.Insert(xpEvent, connection, transaction);
+
+            UserStats? userStats = _userStatsRepository.GetByUserId(task.UserId, connection, transaction) ?? throw new UserStatsNotFoundException(task.UserId);
+            userStats.AddXp(awardXp);
+            _userStatsRepository.UpdateTotalXp(task.UserId, userStats.TotalXp, connection, transaction);
+
+            return awardXp;
         }
 
         public int GetTotalXP(int userId)
