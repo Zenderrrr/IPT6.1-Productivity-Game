6f5526c722d54e48f0f330e1db5c67f2e3d72d26
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-11 01:00:33 +0200

    Update StreakService implement all methods
    implement UpdateStreak, CalculateNewStreak
    
    remove ResetStreakNeeded

--- Project/Backend/FocusUp/Application/Services/StreakService.cs
+++ Project/Backend/FocusUp/Application/Services/StreakService.cs
@@ -1,6 +1,7 @@
 ﻿using FocusUp.Application.Strategies.Interfaces;
 using FocusUp.Infrastructure.Repositories;
 using System;
+using FocusUp.Common.Exceptions;
 
 namespace FocusUp.Application.Services
 {
@@ -17,22 +18,36 @@ namespace FocusUp.Application.Services
 
         public void UpdateStreak(int userId, DateTime completedAt)
         {
-            throw new NotImplementedException();
+            var userStats = _userStatsRepository.GetByUserId(userId) ?? throw new UserStatsNotFoundException(userId);
+
+            if (_streakStrategy.ShouldResetStreak(userStats, completedAt))
+                userStats.ResetStreak();
+            userStats.IncrementStreak(completedAt);
+
+            _userStatsRepository.UpdateStreak(userId, userStats.StreakCount, userStats.BestStreak, completedAt);
         }
 
-        public void ResetStreakNeeded(int userId, DateTime completedAt)
+        public int CalculateNewStreak(UserStats userStats, DateTime completedAt)
         {
-            throw new NotImplementedException();
+            if (_streakStrategy.ShouldResetStreak(userStats, completedAt))
+                return 1;
+
+            if (userStats.StreakLastDate?.Date == completedAt)
+                return userStats.StreakCount;
+
+            return userStats.StreakCount + 1;
         }
 
         public int GetCurrentStreak(int userId)
         {
-            throw new NotImplementedException();
+            var userStats = _userStatsRepository.GetByUserId(userId) ?? throw new UserStatsNotFoundException(userId);
+            return userStats.StreakCount;
         }
 
         public int GetBestStreak(int userId)
         {
-            throw new NotImplementedException();
+            var userStats = _userStatsRepository.GetByUserId(userId) ?? throw new UserStatsNotFoundException(userId);
+            return userStats.BestStreak;
         }
     }
 }
