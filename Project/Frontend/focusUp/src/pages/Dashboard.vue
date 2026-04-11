b436d716811668ad012cdf2445100a330558aa59
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-11 21:03:34 +0200

    Update Dashboard add Last completed tasks & quick actions

--- Project/Frontend/focusUp/src/pages/Dashboard.vue
+++ Project/Frontend/focusUp/src/pages/Dashboard.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import NavAuth from '@/components/layout/NavAuth.vue'
 import StatsOverviewCard from '@/components/ui/StatsOverviewCard.vue'
+import StatsCompletedTask from '@/components/ui/StatsCompletedTask.vue'
+import CurrentWeekStats from '@/components/ui/CurrentWeekStats.vue'
 </script>
 
 <template>
@@ -8,7 +10,7 @@ import StatsOverviewCard from '@/components/ui/StatsOverviewCard.vue'
   <main class="w-full max-w-[64rem] mx-auto  my-10">
 
     <!-- Greeting Section-->
-    <section class="w-full mb-5">
+    <section class="">
       <header class="flex items-center justify-between">
         <div>
           <h1 class="font-bold text-3xl tracking-wide">Willkommen zurück,
@@ -49,6 +51,67 @@ import StatsOverviewCard from '@/components/ui/StatsOverviewCard.vue'
       </div>
     </section>
 
+    <!-- Last completed tasks & quick actions-->
+    <section class="grid grid-cols-6 gap-4">
+      <!-- Title -->
+      <div class="col-span-4 bg-[var(--surface-color)] gen-padding rounded-2xl">
+        <div class="flex items-center justify-between">
+          <span class="font-semibold text-lg">Letzte erledigte Tasks</span>
+          <div class="flex items-center justify-center gap-1 text-[var(--primary-color)] text-sm font-semibold">
+            <span>Alle ansehen</span>
+            <div>
+              <i class="fa-solid fa-arrow-right"></i>
+            </div>
+          </div>
+        </div>
 
+        <!-- Tasks -->
+        <div class="grid grid-cols-1 grid-rows-5 gap-3 mt-4">
+          <div v-for="i in 5" :key="i">
+            <StatsCompletedTask title="Landing Page Navbar fertigstellen" :date="new Date(2026, 3, 6, 20, 9, 54)" :xp="53"></StatsCompletedTask>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-span-2 bg-[var(--surface-color)] rounded-2xl gen-padding">
+        <span class="font-bold ">Schnelle Aktionen</span>
+
+        <!-- Quick Actions-->
+        <div class="mb-4">
+          <div class="flex items-center justify-start mt-4 gap-2 bg-linear-to-r from-[var(--primary-color)] to-[var(--secondary-color)] text-[var(--text-color-white)] rounded-xl px-4 py-4 cursor-pointer">
+            <div class="w-[30px] h-[30px] flex items-center justify-center text-[var(--primary-color-white)] bg-white/20 backdrop-blur-2lg rounded-lg">
+              <i class="fa-solid fa-plus text-sm"></i>
+            </div>
+            <span class="font-semibold">Task erstellen</span>
+          </div>
+
+          <div class="flex items-center justify-start mt-4 gap-2 bg-[var(--background-color)] border border-gray-200 rounded-xl px-4 py-4 cursor-pointer">
+            <div class="w-[30px] h-[30px] flex items-center justify-center rounded-lg bg-[var(--text-color-white)]">
+              <i class="fa-solid fa-list-check text-sm"></i>
+            </div>
+            <span class="font-semibold">Tasks anzeigen</span>
+          </div>
+        </div>
+
+        <!-- Stats Current Week-->
+        <span class="uppercase text-[var(--text-color-light)] text-sm font-semibold">Diese Woche</span>
+        <div class="grid grid-cols-2 grid-rows-2 gap-3 mt-4">
+          <CurrentWeekStats :stats-value="23" name="Tasks"></CurrentWeekStats>
+          <CurrentWeekStats :stats-value="8.4" name="Fokus" digit="h"></CurrentWeekStats>
+          <CurrentWeekStats :stats-value="340" name="XP Gewonnen" digit="+" color-tailwind="text-[var(--primary-color)]"></CurrentWeekStats>
+          <CurrentWeekStats :stats-value="79" name="Ziel erreicht" digit="%" color-tailwind="text-[var(--accent-color)]"></CurrentWeekStats>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
+
+<style scoped>
+  section{
+    margin-bottom: calc(var(--spacing) * 5);
+  }
+
+  .gen-padding{
+    padding: calc(var(--spacing) * 5) calc(var(--spacing) * 7);
+  }
+</style>
