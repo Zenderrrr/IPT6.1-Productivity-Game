eb203089edbe5cd21f03d62a07b143b7288fbc24
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-11 21:01:43 +0200

    Create CurrentWeekStats.vue
    define props
    implemented with tailwind and typescript

---
+++ Project/Frontend/focusUp/src/components/ui/CurrentWeekStats.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+  import { formatNumber } from '@/utils/helpers.ts'
+
+  const props = defineProps<{
+    statsValue: number,
+    name: string,
+    colorTailwind?: string,
+    digit?: string,
+  }>();
+</script>
+
+<template>
+  <div :class="colorTailwind" class="flex flex-col items-center justify-center bg-[var(--background-color)] rounded-lg px-3 py-2">
+    <span class="font-bold text-xl">{{ formatNumber(props.statsValue) }} {{ props.digit }}</span>
+    <span class="font-normal text-xs text-[var(--text-color-light)]">{{ props.name }}</span>
+  </div>
+</template>
