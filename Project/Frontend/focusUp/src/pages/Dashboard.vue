aa3878d23192546d54d26e8b69b8aba792aacfde
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-11 17:39:23 +0200

    add minimum requirement for router

---
+++ Project/Frontend/focusUp/src/pages/Dashboard.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+
+import NavAuth from '@/components/layout/navAuth.vue'
+</script>
+
+<template>
+  <NavAuth nameInitials="SS"></NavAuth>
+</template>
---
+++ Project/Frontend/focusUp/src/pages/Profile.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+
+import NavAuth from '@/components/layout/navAuth.vue'
+</script>
+
+<template>
+  <NavAuth></NavAuth>
+</template>
