55b81e40d0fc09e7f8fec52356aece419cb5ce33
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-11 17:37:23 +0200

    Update index.ts add all routes and increased visibility

--- Project/Frontend/focusUp/src/router/index.ts
+++ Project/Frontend/focusUp/src/router/index.ts
@@ -1,11 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from '../pages/Dashboard.vue'
+import Home from '../pages/Home.vue'
+import Login from '../pages/Auth/Login.vue'
+import Register from '../pages/Auth/Register.vue'
+import Tasks from '@/pages/Tasks.vue'
+import Stats from '@/pages/Stats.vue'
+import Profile from '@/pages/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: () => import('../pages/Home.vue') },
-    { path: '/login', component: () => import('../pages/Auth/Login.vue') },
-    { path: '/signup', component: () => import('../pages/Auth/Register.vue') },
+    {
+      path: '/',
+      component: Home,
+      name: 'Home',
+    },
+    {
+      path: '/login',
+      component: Login,
+      name: 'Login',
+    },
+    {
+      path: '/register',
+      component: Register,
+      name: 'Register',
+    },
+    {
+      path: '/dashboard',
+      component: Dashboard,
+      name: 'Dashboard',
+    },
+    {
+      path: '/tasks',
+      component: Tasks,
+      name: 'Tasks',
+    },
+    {
+      path: '/stats',
+      component: Stats,
+      name: 'Stats',
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      name: 'Profile',
+    }
   ],
 })
 
