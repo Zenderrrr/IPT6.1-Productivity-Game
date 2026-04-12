0c37d7ccab68c9416bf6245d4b1f4b5278665949
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-12 17:16:38 +0200

    Update Logo component fix bug with routerlink

--- Project/Frontend/focusUp/src/components/ui/Logo.vue
+++ Project/Frontend/focusUp/src/components/ui/Logo.vue
@@ -1,9 +1,15 @@
 <script lang="ts" setup>
-
+  const props = defineProps<{
+    link: string
+  }>()
 </script>
 
 <template>
-  <RouterLink class="font-bold" to="/">
+  <div class="bg-[var(--surface-color)]">
+  <RouterLink :to="props.link">
+    <span class="font-bold text-[var(--text-color)]">
     Focus<span class="text-[var(--primary-color)]">Up</span>
+    </span>
   </RouterLink>
+  </div>
 </template>
