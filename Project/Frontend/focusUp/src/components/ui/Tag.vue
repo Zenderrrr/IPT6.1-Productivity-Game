40da96b1395c0b8f48f9c1913c85af212262ef1b
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-12 17:17:14 +0200

    Create Tag component for tasks page

---
+++ Project/Frontend/focusUp/src/components/ui/Tag.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  name: string
+  colorHex: string
+  textColorHex: string
+}>()
+</script>
+
+<template>
+  <div class="tag rounded-lg px-2 py-1 text-xs font-semibold" :style="{ background: props.colorHex, color: props.textColorHex }">
+    <span>{{ props.name }}</span>
+  </div>
+</template>
