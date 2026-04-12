89c807d4a05f5fbe50090bd630af8131ef7b548b
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-12 02:17:28 +0200

    Create Logo.vue component

---
+++ Project/Frontend/focusUp/src/components/ui/Logo.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <RouterLink class="font-bold" to="/">
+    Focus<span class="text-[var(--primary-color)]">Up</span>
+  </RouterLink>
+</template>
