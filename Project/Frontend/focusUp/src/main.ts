0f2d20de0bc704cfc34a3e8192331853856f2f27
Egor <125749439+Zenderrrr@users.noreply.github.com>
2026-04-12 19:20:59 +0200

    import font awesome

--- Project/Frontend/focusUp/src/main.ts
+++ Project/Frontend/focusUp/src/main.ts
@@ -8,9 +8,39 @@ import './styles/base.css'
 import './styles/variables.css'
 import './styles/components.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faClipboardList,
+  faStar,
+  faFire,
+  faBolt,
+  faLightbulb,
+  faBullseye,
+  faArrowTrendUp,
+  faPlay,
+  faPlus,
+  faTrophy,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faClipboardList,
+  faStar,
+  faFire,
+  faBolt,
+  faLightbulb,
+  faBullseye,
+  faArrowTrendUp,
+  faPlay,
+  faPlus,
+  faTrophy,
+)
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
