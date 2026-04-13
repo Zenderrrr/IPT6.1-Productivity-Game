8e6feb8cfd48bb7a2cf56e197d4547ab2851c1c6
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-13 02:31:06 +0200

    Update auth.api.ts add login, register, logout and meApi

---
+++ Project/Frontend/focusUp/src/api/auth.api.ts
@@ -0,0 +1,38 @@
+import { apiFetch } from '@/api/api.ts'
+import type { User } from '@/types/user.ts'
+
+export async function loginApi(usernameOrEmail: string, password: string): Promise<void> {
+  return apiFetch('/auth/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      UsernameOrEmail: usernameOrEmail,
+      Password: password
+    })
+  })
+}
+
+export async function registerApi(username: string, email: string, password: string) : Promise<void> {
+  return apiFetch('/auth/register', {
+    method: 'POST',
+    body: JSON.stringify({
+      Username : username,
+      Email : email,
+      Password: password
+    })
+  })
+}
+
+export async function logoutApi(): Promise<void> {
+  return apiFetch('/auth/logout', {
+
+  })
+}
+
+export async function meApi(token: string) : Promise<User> {
+  return apiFetch<User>('/auth/me', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+}
