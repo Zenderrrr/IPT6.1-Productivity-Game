2b00791c170fb1fcab7a38b4c4043ac5c3829760
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-13 02:32:53 +0200

    Update index.ts add requiresAuth to router

--- Project/Frontend/focusUp/src/router/index.ts
+++ Project/Frontend/focusUp/src/router/index.ts
@@ -6,6 +6,7 @@ import Register from '../pages/Auth/Register.vue'
 import Tasks from '@/pages/Tasks.vue'
 import Stats from '@/pages/Stats.vue'
 import Profile from '@/pages/Profile.vue'
+import { useAuthStore } from '@/stores/authStore.ts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,38 +15,57 @@ const router = createRouter({
       path: '/',
       component: Home,
       name: 'Home',
+      meta: { requiresAuth: false },
     },
     {
       path: '/login',
       component: Login,
       name: 'Login',
+      meta: { requiresAuth: false },
     },
     {
       path: '/register',
       component: Register,
       name: 'Register',
+      meta: { requiresAuth: false },
     },
     {
       path: '/dashboard',
       component: Dashboard,
       name: 'Dashboard',
+      meta: { requiresAuth: true },
     },
     {
       path: '/tasks',
       component: Tasks,
       name: 'Tasks',
+      meta: { requiresAuth: true },
     },
     {
       path: '/stats',
       component: Stats,
       name: 'Stats',
+      meta: { requiresAuth: true },
     },
     {
       path: '/profile',
       component: Profile,
       name: 'Profile',
+      meta: { requiresAuth: true },
     }
   ],
+})
+
+router.beforeEach(async (to) => {
+  const authStore = useAuthStore()
+
+  if(!authStore.user && !authStore.token) {
+    await authStore.me()
+  }
+
+  if(to.meta.requiresAuth && !authStore.isAuth) {
+    return '/login'
+  }
 })
 
 export default router
