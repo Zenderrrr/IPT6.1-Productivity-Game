291b8d73fdab6caaaff07c2ef0863203ebeaa657
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-14 20:56:53 +0200

    Update api.ts fix bug with options & better error return

--- Project/Frontend/focusUp/src/api/api.ts
+++ Project/Frontend/focusUp/src/api/api.ts
@@ -2,15 +2,20 @@ const API_URL = "http://localhost:5165/api";
 
 export async function apiFetch<T>(endpoint: string, options?: RequestInit): Promise<T> {
   const res = await fetch(`${API_URL}${endpoint}`, {
+    ...options,
     headers: {
       "Content-Type": "application/json",
       ...(options?.headers || {})
     },
-    ...options
   });
 
-  if(!res.ok){
-    throw new Error(`Error: ${res.status}`)
+  if (!res.ok) {
+    const text = await res.text()
+
+    console.log(`Error ${res.status}: ${text}`)
+
+    throw new Error(`Error ${res.status}: ${text}`)
   }
+
   return res.json()
 }
