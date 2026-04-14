d085b0bf3fac119553eec34b55bdc905c72bfc53
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-14 02:18:02 +0200

    Update TaskComponent change standard behavior & add prop

--- Project/Frontend/focusUp/src/components/ui/TasksComponent.vue
+++ Project/Frontend/focusUp/src/components/ui/TasksComponent.vue
@@ -7,23 +7,28 @@ const props = defineProps<{
   taskTitle: string
   taskDescription: string
   timeMin: number
-  date: Date
+  xp: number
+  date?: Date
+  completed: boolean
 }>()
 
 const month = Intl.DateTimeFormat('en-US', { month: 'short' }).format(props.date)
 
 // task finished logic
-const isChecked = ref<boolean>(false);
+const isChecked = ref<boolean>(props.completed)
 
-function changeChecked(){
-  isChecked.value = !isChecked.value;
+function changeChecked() {
+  isChecked.value = !isChecked.value
 }
 </script>
 
 <template>
-  <div :class="isChecked ? 'checked' : 'unchecked'" class="cursor-pointer base-element w-full flex items-center justify-start gap-4">
+  <div
+    :class="isChecked ? 'checked' : 'unchecked'"
+    class="cursor-pointer base-element w-full flex items-center justify-start gap-4"
+  >
     <label class="container -translate-y-2.5">
-      <input @click="changeChecked" type="checkbox"/>
+      <input @click="changeChecked" type="checkbox" />
       <span class="checkmark rounded-2xl"></span>
     </label>
 
@@ -31,7 +36,6 @@ function changeChecked(){
       <span class="title font-semibold">{{ props.taskTitle }}</span>
       <span class="text-[var(--text-color-light)] text-sm">{{ props.taskDescription }}</span>
       <div class="mt-1 flex items-center justify-start gap-2">
-
         <!-- Tags-->
         <slot></slot>
 
@@ -48,14 +52,14 @@ function changeChecked(){
           class="flex gap-1 items-center justify-center rounded-lg px-2 py-1 text-xs font-semibold bg-gray-100 text-[var(--text-color-light)]"
         >
           <i class="fa-regular fa-calendar"></i>
-          <span>{{ `${formatTime(props.date.getDate())}. ${month}.`}}</span>
+          <span>{{ `${formatTime(props.date.getDate())}. ${month}.` }}</span>
         </div>
 
         <!-- XP-->
         <div
           class="flex gap-1 items-center justify-center rounded-lg px-2 py-1 text-xs font-semibold bg-gray-100 text-[var(--text-color-light)]"
         >
-          <span class="text-[var(--primary-color)]">+ 50 XP</span>
+          <span class="text-[var(--primary-color)]">+ {{ props.xp }} XP</span>
         </div>
       </div>
     </div>
@@ -63,18 +67,18 @@ function changeChecked(){
 </template>
 
 <style scoped>
-  .checked{
-    background-color: oklch(96.7% 0.003 264.542);
-  }
+.checked {
+  background-color: oklch(96.7% 0.003 264.542);
+}
 
-  .checked .title{
-    text-decoration: line-through;
-    color: var(--text-color-light);
-  }
+.checked .title {
+  text-decoration: line-through;
+  color: var(--text-color-light);
+}
 
-  .unchecked{
-    text-decoration: none;
-  }
+.unchecked {
+  text-decoration: none;
+}
 
 /* Customize the label (the container) */
 .container {
@@ -123,7 +127,7 @@ function changeChecked(){
 
 /* Create the checkmark/indicator (hidden when not checked) */
 .checkmark:after {
-  content: "";
+  content: '';
   position: absolute;
   display: none;
 }
