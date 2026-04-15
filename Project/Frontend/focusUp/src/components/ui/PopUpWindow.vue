d520be4e1b18efdfe4662806c3d9e85627926aaf
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-15 02:06:16 +0200

    Create PopUpWindow basic structure for pop-ups

---
+++ Project/Frontend/focusUp/src/components/ui/PopUpWindow.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+  const emit = defineEmits<{
+    (e: 'cancel') : void
+    (e: 'submit') : void
+  }>();
+
+  const props = defineProps<{
+    title: string,
+  }>()
+
+  function cancel() {
+    emit('cancel');
+  }
+</script>
+
+<template>
+  <div class="absolute w-[100vw] h-[100vh] flex justify-center items-center backdrop-blur-xs z-999">
+    <div class="base-element">
+      <!-- Title and close-->
+      <div class="flex items-center justify-between w-[500px]">
+        <span class="font-semibold text-xl">{{ props.title }}</span>
+        <button @click="cancel" class="flex justify-center items-center bg-gray-100 text-gray-400 w-[35px] h-[35px] rounded-lg text-sm cursor-pointer">
+          <i class="fa-solid fa-x"></i>
+        </button>
+      </div>
+
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
