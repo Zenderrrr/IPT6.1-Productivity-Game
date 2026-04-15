1be25b45031076d549988327c21c1501b0b9e2ec
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-16 01:13:29 +0200

    Update PopUpWindow add button animation

--- Project/Frontend/focusUp/src/components/ui/PopUpWindow.vue
+++ Project/Frontend/focusUp/src/components/ui/PopUpWindow.vue
@@ -14,18 +14,18 @@
 </script>
 
 <template>
-  <div class="absolute w-[100vw] h-[100vh] flex justify-center items-center backdrop-blur-xs z-999">
-    <div class="base-element">
-      <!-- Title and close-->
-      <div class="flex items-center justify-between w-[500px]">
-        <span class="font-semibold text-xl">{{ props.title }}</span>
-        <button @click="cancel" class="flex justify-center items-center bg-gray-100 text-gray-400 w-[35px] h-[35px] rounded-lg text-sm cursor-pointer">
-          <i class="fa-solid fa-x"></i>
-        </button>
-      </div>
+    <div class="absolute w-[100vw] h-[100vh] flex justify-center items-center backdrop-blur-xs z-999">
+      <div class="base-element">
+        <!-- Title and close-->
+        <div class="flex items-center justify-between w-[500px]">
+          <span class="font-semibold text-xl">{{ props.title }}</span>
+          <button @click="cancel" class="hover:border-[var(--primary-color)] hover:text-[var(--primary-color)] duration-200 transition border-1 border-transparent flex justify-center items-center bg-gray-100 text-gray-400 w-[35px] h-[35px] rounded-lg text-sm cursor-pointer">
+            <i class="fa-solid fa-x"></i>
+          </button>
+        </div>
 
-      <slot></slot>
+        <slot></slot>
+      </div>
     </div>
-  </div>
 </template>
 
