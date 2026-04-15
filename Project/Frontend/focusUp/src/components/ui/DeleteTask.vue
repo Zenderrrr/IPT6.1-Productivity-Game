eb96007814d5554f9a3df2e6a6af7021ce4dd05b
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-15 02:05:39 +0200

    Create DeleteTask component as a pop-up

---
+++ Project/Frontend/focusUp/src/components/ui/DeleteTask.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import PopUpWindow from '@/components/ui/PopUpWindow.vue'
+
+const props = defineProps<{
+  isShown: boolean,
+  taskId: number
+}>()
+
+const emit = defineEmits<{
+  (e: 'cancel'): void
+  (e: 'confirm', id: number): void
+}>()
+
+function cancel() {
+  emit('cancel')
+}
+
+function confirm() {
+  emit('confirm', props.taskId)
+}
+</script>
+
+<template>
+  <PopUpWindow :style="props.isShown ? '' : 'display: none' " @cancel="cancel" title="Wirklich löschen?">
+    <div class="w-full flex justify-center items-center my-6">
+      <button @click="confirm" class="cursor-pointer px-4 py-2 bg-red-100 border border-red-500 text-red-500 rounded-lg">Endgültig Löschen</button>
+    </div>
+  </PopUpWindow>
+</template>
