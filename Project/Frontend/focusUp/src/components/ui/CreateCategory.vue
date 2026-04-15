b9b3461f823f8fe34378273975d5b4b544f051b5
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-15 02:05:21 +0200

    Create CreateCategory component implement as a pop-up

---
+++ Project/Frontend/focusUp/src/components/ui/CreateCategory.vue
@@ -0,0 +1,146 @@
+<script lang="ts" setup>
+import PopUpWindow from '@/components/ui/PopUpWindow.vue'
+import { ref } from 'vue'
+import type { CreateCategory} from '@/types/createCategory.ts'
+
+const props = defineProps<{
+  isShown: boolean,
+}>()
+
+const emit = defineEmits<{
+  (e: 'submit', category: CreateCategory): void
+  (e: 'cancel'): void
+}>()
+
+function cancel() {
+  emit('cancel')
+}
+
+const choosenColor = ref<string>()
+const name = ref<string>()
+
+const ownChoosenColor = ref<string>()
+function submit() {
+  if (choosenColor.value == null) {
+    return
+  }
+
+  if (name.value == null) {
+    return
+  }
+
+  const category: CreateCategory = {
+    name: name.value,
+    color: choosenColor.value,
+  }
+
+  emit('submit', category)
+}
+
+const colors = [
+  '#efb7b0',
+  '#f1d39f',
+  '#f0e17f',
+  '#e9eb66',
+  '#bff07a',
+  '#9fe5be',
+  '#8fdcc4',
+  '#a7d8f0',
+  '#96cbf0',
+  '#8dbef0',
+  '#b3b0f3',
+  '#c4b0f0',
+  '#d5acef',
+  '#e6a3ea',
+  '#e8a7c8',
+  '#edb3b8',
+]
+</script>
+
+<template>
+  <PopUpWindow :style="props.isShown ? '' : 'display: none' ">
+    <!-- Title and close-->
+    <div class="flex items-center justify-between w-[500px]">
+      <span class="font-semibold text-xl">Erstelle eine neue Kategorie</span>
+      <button
+        @click="cancel"
+        class="flex justify-center items-center bg-gray-100 text-gray-400 w-[35px] h-[35px] rounded-lg text-sm cursor-pointer"
+      >
+        <i class="fa-solid fa-x"></i>
+      </button>
+    </div>
+
+    <!-- Inputs-->
+    <form @submit.prevent="submit" class="w-full flex flex-col gap-5 mt-6">
+      <div class="flex items-start justify-center flex-col gap-1.5">
+        <label class="text-sm font-semibold ml-0.5" for="title">Name *</label>
+        <input
+          required
+          v-model="name"
+          class="w-full px-4 py-3 border border-gray-200 rounded-lg outline-[var(--primary-color)]"
+          id="title"
+          type="text"
+          placeholder="zb. Hobby, Arbeit, Fitness, Lernen ..."
+        />
+      </div>
+
+      <div class="flex items-start justify-center flex-col gap-1.5">
+        <label class="text-sm font-semibold ml-0.5" for="title">Farbe *</label>
+
+        <div class="grid grid-cols-10 gap-1.5">
+          <div
+            @click="choosenColor = color"
+            :class="choosenColor === color ? 'category-active' : ''"
+            v-for="color in colors"
+            :style="{ backgroundColor: color }"
+            class="scale-animation cursor-pointer border-2 border-transparent bg-[var(--primary-color)] rounded-lg w-[40px] h-[40px]"
+          >
+            <div
+              v-if="choosenColor === color"
+              class="w-full h-full flex justify-center items-center text-[var(--text-color-white)] text-sm"
+            >
+              <i class="fa-solid fa-check"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-start justify-center flex-col gap-1.5">
+        <label class="text-sm font-semibold ml-0.5" for="title">Eigene Farbe</label>
+        <input
+          v-model="ownChoosenColor"
+          @click="choosenColor = ownChoosenColor"
+          class="h-[50px] cursor-pointer w-full px-4 py-3 border border-gray-200 rounded-lg outline-[var(--primary-color)]"
+          id="title"
+          type="color"
+          placeholder="zb. Hobby, Arbeit, Fitness, Lernen ..."
+        />
+      </div>
+
+      <div class="h-0.5 bg-gray-100 w-full"></div>
+
+      <!-- submit & cancel-->
+      <div class="w-full flex justify-end items-center gap-3">
+        <button
+          @click="cancel"
+          class="cursor-pointer px-4 py-2 bg-transparent border border-gray-200 rounded-lg text-[var(--text-color-light)] font-semibold"
+        >
+          Cancel
+        </button>
+        <button
+          class="cursor-pointer px-4 py-2 bg-linear-to-r from-[var(--primary-color)] to-[var(--secondary-color)] border rounded-lg text-[var(--text-color-white)] font-semibold"
+        >
+          Kategorie erstellen
+        </button>
+      </div>
+    </form>
+  </PopUpWindow>
+</template>
+
+<style scoped>
+.category-active {
+  border-color: oklch(55.1% 0.027 264.364);
+  width: 105%;
+  height: 105%;
+}
+</style>
