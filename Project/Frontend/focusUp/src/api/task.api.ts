d351caa6ecc1ea15b49aa484b1839ae9c02bd454
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-16 01:10:47 +0200

    Update task.api input / add completeTaskApi

--- Project/Frontend/focusUp/src/api/task.api.ts
+++ Project/Frontend/focusUp/src/api/task.api.ts
@@ -1,8 +1,9 @@
 import type { Task } from '@/types/task.ts'
 import { apiFetch } from '@/api/api.ts'
-import type { CreateTask } from '@/types/createTask.ts'
+import type { CreateTaskType } from '@/types/createTaskType.ts'
+import type { UpdateTask } from '@/types/updateTask.ts'
 
-export async function createTaskApi(task: CreateTask): Promise<number> {
+export async function createTaskApi(task: CreateTaskType): Promise<number> {
   return await apiFetch('/tasks', {
     method: 'POST',
     body: JSON.stringify(task),
@@ -12,8 +13,8 @@ export async function createTaskApi(task: CreateTask): Promise<number> {
   })
 }
 
-export async function updateTaskApi(task: Task): Promise<void> {
-  await apiFetch(`/tasks/${task.id}`, {
+export async function updateTaskApi(taskId: number, task: UpdateTask): Promise<Task> {
+  return await apiFetch(`/tasks/${taskId}`, {
     method: 'PUT',
     body: JSON.stringify(task),
     headers: {
@@ -43,6 +44,15 @@ export async function getAllTasksApi(): Promise<Task[]> {
 export async function getTaskByIdApi(id: number): Promise<Task> {
   return await apiFetch(`/tasks/${id}`, {
     method: 'GET',
+    headers: {
+      Authorization: `BEARER ${localStorage.getItem('token')}`,
+    }
+  })
+}
+
+export async function completeTaskApi(id: number): Promise<void> {
+  return await apiFetch(`/tasks/${id}/complete`, {
+    method: 'POST',
     headers: {
       Authorization: `BEARER ${localStorage.getItem('token')}`,
     }
