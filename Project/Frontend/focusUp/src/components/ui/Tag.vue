9178f8691b711acfc8a34b7b70fe04a5fd3350cb
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-15 02:06:39 +0200

    Update Tag.vue add dynamic styles

--- Project/Frontend/focusUp/src/components/ui/Tag.vue
+++ Project/Frontend/focusUp/src/components/ui/Tag.vue
@@ -1,13 +1,14 @@
 <script lang="ts" setup>
 const props = defineProps<{
-  name: string
-  colorHex: string
-  textColorHex: string
+  name: string,
+  colorHex: string,
+  textColorHex: string,
+  borderColorHex?: string
 }>()
 </script>
 
 <template>
-  <div class="tag rounded-lg px-2 py-1 text-xs font-semibold" :style="{ background: props.colorHex, color: props.textColorHex }">
+  <div class="tag rounded-lg px-2 py-1 text-xs font-semibold border border-[var(--primary-color)]" :style="{ background: props.colorHex, color: props.textColorHex, ...(props.borderColorHex ? { borderColor: props.borderColorHex } : { borderColor: 'transparent' })}">
     <span>{{ props.name }}</span>
   </div>
 </template>
