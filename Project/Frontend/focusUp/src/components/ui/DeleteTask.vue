0d04157cf661c59a6abc5a5f4d535044ff129373
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-16 01:13:06 +0200

    Update DeleteTask add animation

--- Project/Frontend/focusUp/src/components/ui/DeleteTask.vue
+++ Project/Frontend/focusUp/src/components/ui/DeleteTask.vue
@@ -23,7 +23,7 @@ function confirm() {
 <template>
   <PopUpWindow :style="props.isShown ? '' : 'display: none' " @cancel="cancel" title="Wirklich löschen?">
     <div class="w-full flex justify-center items-center my-6">
-      <button @click="confirm" class="cursor-pointer px-4 py-2 bg-red-100 border border-red-500 text-red-500 rounded-lg">Endgültig Löschen</button>
+      <button @click="confirm" class="scale-animation-sm hover:bg-transparent transition duration-100 cursor-pointer px-4 py-2 bg-red-100 border border-red-500 text-red-500 rounded-lg">Endgültig Löschen</button>
     </div>
   </PopUpWindow>
 </template>
