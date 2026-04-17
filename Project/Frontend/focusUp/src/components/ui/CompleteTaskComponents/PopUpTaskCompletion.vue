f702866bf297b21c3380b166250b0c8b1c03da30
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-17 19:16:42 +0200

    Create PopUpTaskCompletion.vue

---
+++ Project/Frontend/focusUp/src/components/ui/CompleteTaskComponents/PopUpTaskCompletion.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="absolute w-[100vw] h-[100vh] flex justify-center items-center backdrop-blur-xs z-999">
+      <slot></slot>
+  </div>
+</template>
