78b9504f517f3bda68fe2429d68b5696d9a7251c
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-20 00:44:33 +0200

    Create UserRefreshToken entity

---
+++ Project/Backend/FocusUp/Domain/Models/UserRefreshToken.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace FocusUp.Domain.Models
+{
+    public class UserRefreshToken : BaseModel
+    {
+        public int UserId { get; private set; }
+        public string TokenHash { get; private set; }
+        public DateTime ExpiresAt { get; private set; }
+        public DateTime? RevokedAt { get; private set; }
+
+        public UserRefreshToken(int id, int userId, string tokenHash, DateTime createdAt, DateTime expiresAt, DateTime? revokedAt = null)
+        {
+            Id = id;
+            UserId = userId;
+            TokenHash = tokenHash;
+            ExpiresAt = expiresAt;
+            RevokedAt = revokedAt;
+            CreatedAt = createdAt;
+        }
+
+        public UserRefreshToken(int userId, string tokenHash, DateTime expiresAt)
+        {
+            UserId = userId;
+            TokenHash = tokenHash;
+            ExpiresAt = expiresAt;
+        }
+
+        public void SetRevokedAt(DateTime date) => RevokedAt = date;
+    }
+}
