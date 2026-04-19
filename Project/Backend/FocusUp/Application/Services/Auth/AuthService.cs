0a15a000ae50ffcf177f636f5f2fbb639c009973
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-20 00:42:46 +0200

    Update AuthService add refreshToken functions

--- Project/Backend/FocusUp/Application/Services/Auth/AuthService.cs
+++ Project/Backend/FocusUp/Application/Services/Auth/AuthService.cs
@@ -2,6 +2,10 @@ using FocusUp.Infrastructure.Repositories;
 using FocusUp.Application.Services;
 using System;
 using System.IO;
+using FocusUp.Domain.Models;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace FocusUp.Application.Services.Auth
 {
@@ -11,15 +15,17 @@ namespace FocusUp.Application.Services.Auth
         private readonly UserStatsRepository _userStatsRepository;
         private readonly JwtTokenService _jwtTokenService;
         private readonly PasswordHasher _passwordHasher;
+        private readonly UserRefreshTokenRepository _userRefreshTokenRepository;
 
         private readonly DatabaseConnection _databaseConnection;
 
-        public AuthService(UserRepository userRepository, UserStatsRepository userStatsRepository, JwtTokenService jwtTokenService, PasswordHasher passwordHasher, DatabaseConnection databaseConnection)
+        public AuthService(UserRepository userRepository, UserStatsRepository userStatsRepository, JwtTokenService jwtTokenService, PasswordHasher passwordHasher, DatabaseConnection databaseConnection, UserRefreshTokenRepository userRefreshTokenRepository)
         {
             _userRepository = userRepository;
             _userStatsRepository = userStatsRepository;
             _jwtTokenService = jwtTokenService;
             _passwordHasher = passwordHasher;
+            _userRefreshTokenRepository = userRefreshTokenRepository;
 
             _databaseConnection = databaseConnection;
         }
@@ -68,6 +74,71 @@ namespace FocusUp.Application.Services.Auth
                 transaction.Rollback();
                 throw;
             }
+        }
+
+        public UserRefreshToken? GetRefreshToken(string refreshToken)
+        {
+            var hashRefreshToken = HashToken(refreshToken);
+
+            var userRefreshToken = _userRefreshTokenRepository.GetByToken(hashRefreshToken);
+
+            if(userRefreshToken == null)
+                return null;
+            if (userRefreshToken.RevokedAt != null)
+                return null;
+            if(userRefreshToken.ExpiresAt < DateTime.UtcNow)
+                return null;
+            return userRefreshToken;
+        }
+
+        public (UserRefreshToken token, string plainToken) CreateRefreshToken(int userId)
+        {
+            var plainToken = GenerateSecureToken();
+            var tokenHash = HashToken(plainToken);
+
+            var newToken = new UserRefreshToken(
+                    userId,
+                    tokenHash,
+                    DateTime.UtcNow.AddDays(30)
+                );
+
+            _userRefreshTokenRepository.Insert(newToken);
+            return (newToken, plainToken);
+        }
+
+        public (UserRefreshToken token, string plainToken) RotateRefreshToken(string refreshToken)
+        {
+            var oldToken = GetRefreshToken(refreshToken) ?? throw new Exception("Token not found.");
+
+            oldToken.SetRevokedAt(DateTime.UtcNow);
+            _userRefreshTokenRepository.UpdateRevokedAt(oldToken);
+
+            var plainToken = GenerateSecureToken();
+            var tokenHash = HashToken(plainToken);
+
+            var newToken = new UserRefreshToken(
+                    oldToken.UserId,
+                    tokenHash,
+                    DateTime.UtcNow.AddDays(30)
+                );
+
+            _userRefreshTokenRepository.Insert(newToken);
+            return (newToken, plainToken);
+        }
+
+        private string GenerateSecureToken()
+        {
+            var randomBytes = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomBytes);
+            return Base64UrlEncoder.Encode(randomBytes);
+        }
+
+        private string HashToken(string token)
+        {
+            var bytes = Encoding.UTF8.GetBytes(token);
+            var hash = SHA256.HashData(bytes);
+            return Convert.ToHexString(hash);
         }
     }
 }
