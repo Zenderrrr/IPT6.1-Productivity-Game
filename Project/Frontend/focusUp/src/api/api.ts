f71a82a9c5f9f2ef550edc1d6374a591dd2a0f35
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-21 00:15:21 +0200

    Update api.ts add refresh function

--- Project/Frontend/focusUp/src/api/api.ts
+++ Project/Frontend/focusUp/src/api/api.ts
@@ -1,13 +1,38 @@
+import { useAuthStore } from '@/stores/authStore.ts'
+
 const API_URL = "http://localhost:5165/api";
 
 export async function apiFetch<T>(endpoint: string, options?: RequestInit): Promise<T> {
-  const res = await fetch(`${API_URL}${endpoint}`, {
+  const authStore = useAuthStore()
+
+  let res = await fetch(`${API_URL}${endpoint}`, {
     ...options,
     headers: {
       "Content-Type": "application/json",
+      ...(authStore.token ? { Authorization: `Bearer ${authStore.token}` } : {}),
       ...(options?.headers || {})
     },
+    credentials: 'include'
   });
+
+  if(res.status === 401){
+    try {
+      await authStore.refresh()
+
+      res = await fetch(`${API_URL}${endpoint}`, {
+        ...options,
+        headers: {
+          "Content-Type": "application/json",
+          ...(authStore.token ? { Authorization: `Bearer ${authStore.token}` } : {}),
+          ...(options?.headers || {})
+        },
+        credentials: 'include'
+      });
+    }catch{
+      throw new Error(`Session expired`)
+    }
+  }
+
 
   if (!res.ok) {
     const text = await res.text()
