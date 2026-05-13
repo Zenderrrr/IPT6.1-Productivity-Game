dc919ae9b9556494e6ae959d03e4e36f8be01605
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-13 16:09:40 +0200

    Update Categories add delete option

--- Project/Frontend/focusUp/src/components/ui/Categories.vue
+++ Project/Frontend/focusUp/src/components/ui/Categories.vue
@@ -14,8 +14,11 @@
 </script>
 
 <template>
-  <div @click="buttonClicked" :class="props.isActive ? 'active' : '' " class="hover:border-gray-400 transition duration-200 cursor-pointer text-center px-3 py-2 text-sm bg-[var(--surface-color)] text-[var(--text-color-light)] border border-gray-200 text-nowrap rounded-full inline">
+  <div @click="buttonClicked" :class="props.isActive ? 'active' : '' " class="flex items-center justify-center gap-2 hover:border-gray-400 transition duration-200 cursor-pointer text-center px-3 py-2 text-sm bg-[var(--surface-color)] text-[var(--text-color-light)] border border-gray-200 text-nowrap rounded-full">
     <span>{{ props.text }}</span>
+    <div class="hover:bg-gray-100 flex items-center justify-center w-[20px] h-[20px] text-[10px] rounded-full border border-[var(--text-color-light)]">
+      <i class="fa-solid fa-x -translate-x-[.5px]"></i>
+    </div>
   </div>
 </template>
 
