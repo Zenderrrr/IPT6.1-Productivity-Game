c5cc626fcd8d08ddbec80fb1b9d9266cb1b3b6e1
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-14 15:06:00 +0200

    Create PlaceholderLastComplTask.vue

---
+++ Project/Frontend/focusUp/src/components/ui/PlaceholderLastComplTask.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div
+    class="flex gap-2 justify-between items-center bg-[var(--background-color)] px-4 py-3 rounded-2xl"
+  >
+    <div class="flex gap-4 items-center">
+      <div
+        class="animate-pulse w-[22px] h-[22px] flex justify-center items-center text-center bg-gray-200 text-[var(--text-color-white)] text-xs font-light rounded-2xl"
+      >
+        <i class="fa-solid fa-check"></i>
+      </div>
+      <div class="flex flex-col gap-4 font-light">
+        <div class="h-3 w-11 placeholder"></div>
+        <div class="placeholder h-3 w-15"></div>
+      </div>
+    </div>
+    <div
+      class="placeholder w-15 h-6 rounded-2xl! px-2 py-1"
+    >
+    </div>
+  </div>
+</template>
