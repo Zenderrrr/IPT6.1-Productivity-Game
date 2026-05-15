91bceddbed2755c63c5e4e250b695c6a442606ba
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-16 00:23:24 +0200

    Create ConsistencyDeadlineBadgeRule without implementation

---
+++ Project/Backend/FocusUp/Application/Strategies/ConsistencyDeadlineBadgeRule.cs
@@ -0,0 +1,22 @@
+using FocusUp.Application.Strategies.Interfaces;
+using FocusUp.Domain.Enums;
+using System;
+
+namespace FocusUp.Application.Strategies
+{
+    public class ConsistencyDeadlineBadgeRule : IBadgeRule
+    {
+        private readonly BadgeRuleType _ruleType = BadgeRuleType.consistency_check_morning;
+
+        public ConsistencyDeadlineBadgeRule()
+        {
+        }
+
+        public bool IsUnlocked(UserStats stats, Badge badge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BadgeRuleType GetRuleType() => _ruleType;
+    }
+}
