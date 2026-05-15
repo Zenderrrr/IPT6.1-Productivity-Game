c0541e99287e7f315ffaf77cf06851cc47595f77
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-16 00:25:59 +0200

    Update Badge expand attributes

--- Project/Backend/FocusUp/Domain/Models/Badge.cs
+++ Project/Backend/FocusUp/Domain/Models/Badge.cs
@@ -8,29 +8,27 @@ public class Badge : BaseModel
     public string Description { get; private set; }
     public BadgeRuleType RuleType { get; private set; }
     public int RuleValue { get; private set; }
+    public BadgeRarity Rarity { get; private set; }
+    public string PrimaryColor { get; private set; }
+    public string SecondaryColor { get; private set; }
 
     public Badge()
     {
     }
 
-    public Badge(string name, string description, BadgeRuleType ruleType, int ruleValue)
+    public Badge(string name, string description, BadgeRuleType ruleType, int ruleValue, BadgeRarity rarity, string primaryColor, string secondaryColor)
     {
         Name = name;
         Description = description;
         RuleType = ruleType;
         RuleValue = ruleValue;
+        Rarity = rarity;
+        PrimaryColor = primaryColor;
+        SecondaryColor = secondaryColor;
     }
 
-    public void Rename(string newName)
-    {
-        Name = newName;
-    }
-
-    public void NewDescription(string newDescription)
-    {
-        Description = newDescription;
-    }
-
+    public void Rename(string newName) => Name = newName;
+    public void NewDescription(string newDescription) => Description = newDescription;
     public void UpdateRule(BadgeRuleType ruleType, int ruleValue)
     {
         RuleType = ruleType;
@@ -42,6 +40,8 @@ public class Badge : BaseModel
         return
             !string.IsNullOrWhiteSpace(Name) &&
             !string.IsNullOrWhiteSpace(Description) &&
-            !int.IsNegative(RuleValue);
+            !int.IsNegative(RuleValue) &&
+            !string.IsNullOrWhiteSpace(PrimaryColor) &&
+            !string.IsNullOrWhiteSpace(SecondaryColor);
     }
 }
