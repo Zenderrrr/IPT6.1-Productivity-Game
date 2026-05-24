c626f8197cc064e17c70fd1e91de79e6ee6ef436
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-25 00:04:43 +0200

    Update DifficultyHardcoreBadgeRule method

--- Project/Backend/FocusUp/Application/Strategies/DifficultyHardcoreBadgeRule.cs
+++ Project/Backend/FocusUp/Application/Strategies/DifficultyHardcoreBadgeRule.cs
@@ -6,15 +6,23 @@ namespace FocusUp.Application.Strategies
 {
     public class DifficultyHardcoreBadgeRule : IBadgeRule
     {
-        private readonly BadgeRuleType _ruleType = BadgeRuleType.difficulty_achived_easy;
+        private readonly BadgeRuleType _ruleType = BadgeRuleType.difficulty_achived_hardcore;
 
         public DifficultyHardcoreBadgeRule()
         {
         }
 
-        public bool IsUnlocked(UserStats stats, Badge badge)
+        public bool IsUnlocked(BadgeContext context, Badge badge)
         {
-            throw new NotImplementedException();
+            var tasks = context.Tasks
+                .Where(t => t.CompletedAt != null)
+                .OrderBy(t => t.CompletedAt)
+                .Take(badge.RuleValue)
+                .ToList();
+
+            if (tasks.Count < badge.RuleValue) return false;
+
+            return tasks.All(t => t.Difficulty == TaskDifficultyType.Hard);
         }
 
         public BadgeRuleType GetRuleType() => _ruleType;
