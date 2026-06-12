900210ea6f006b9d4e1bfbb1c2238ea42f3ef444
Egor <125749439+Zenderrrr@users.noreply.github.com>
2026-06-12 19:18:51 +0200

    change responsiveness of pop-up window layout

--- Project/Frontend/focusUp/src/components/ui/PopUpWindow.vue
+++ Project/Frontend/focusUp/src/components/ui/PopUpWindow.vue
@@ -14,12 +14,12 @@
 </script>
 
 <template>
-    <div class="fixed inset-0 flex justify-center items-center backdrop-blur-xs z-999">
-      <div class="base-element">
+    <div class="fixed inset-0 flex justify-center items-center backdrop-blur-xs z-999 px-4 py-6">
+      <div class="base-element w-full max-w-[34rem] max-h-[90vh] overflow-y-auto">
         <!-- Title and close-->
-        <div class="flex items-center justify-between min-w-[500px]">
-          <span class="font-semibold text-xl">{{ props.title }}</span>
-          <button @click="cancel" class="hover:border-[var(--primary-color)] hover:text-[var(--primary-color)] duration-200 transition border-1 border-transparent flex justify-center items-center bg-gray-100 text-gray-400 w-[35px] h-[35px] rounded-lg text-sm cursor-pointer">
+        <div class="flex items-center justify-between gap-4 w-full">
+          <span class="font-semibold text-lg sm:text-xl break-words">{{ props.title }}</span>
+          <button @click="cancel" class="shrink-0 hover:border-[var(--primary-color)] hover:text-[var(--primary-color)] duration-200 transition border-1 border-transparent flex justify-center items-center bg-gray-100 text-gray-400 w-[35px] h-[35px] rounded-lg text-sm cursor-pointer">
             <i class="fa-solid fa-x"></i>
           </button>
         </div>
@@ -28,4 +28,3 @@
       </div>
     </div>
 </template>
-
