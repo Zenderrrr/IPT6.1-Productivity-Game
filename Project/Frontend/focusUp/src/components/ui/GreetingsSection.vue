f2be1d7e879c9215a846e8547449745fa7d15066
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-13 20:51:52 +0200

    Update GreetingsSection add responsibility

--- Project/Frontend/focusUp/src/components/ui/GreetingsSection.vue
+++ Project/Frontend/focusUp/src/components/ui/GreetingsSection.vue
@@ -23,7 +23,7 @@ const month = new Intl.DateTimeFormat('de-CH', { month: 'long' }).format(date)
         <p class="text-[var(--text-color-light)] mt-1"> {{ props.subtitle }}</p>
       </div>
       <div
-        class="text-[var(--text-color-light)] text-sm font-semibold px-2.5 py-1.5 bg-[var(--surface-color)] border border-gray-200 rounded-full"
+        class="text-[var(--text-color-light)] text-sm font-semibold px-2.5 py-1.5 bg-[var(--surface-color)] border border-[var(--border-color)] rounded-full"
       >
         {{ ` ${day}, ${formatTime(date.getDate())} ${month} ${date.getFullYear()}` }}
       </div>
