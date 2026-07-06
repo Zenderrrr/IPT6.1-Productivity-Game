e5f0893ed46ba5aebafa347249966f4fa2564fe5
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-07-06 14:18:52 +0200

    Create Landingpage components

---
+++ Project/Frontend/focusUp/src/components/ui/Landingpage/SectionStruct.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  kicker: string
+  title: string
+  subtitle: string
+}>()
+</script>
+
+<template>
+  <section class="p-8">
+    <div class="max-w-[1400px] mx-auto">
+      <div class="text-center">
+        <span class="uppercase text-[var(--primary-color)] text-sm font-semibold">{{
+            props.kicker
+          }}</span>
+        <h2 class="text-[40px] font-extrabold">{{ props.title }}</h2>
+        <div class="w-full flex justify-center">
+          <p class="text-[var(--text-color-light)] text-md max-w-[600px]">
+            {{ props.subtitle }}
+          </p>
+        </div>
+      </div>
+
+      <slot></slot>
+    </div>
+  </section>
+</template>
---
+++ Project/Frontend/focusUp/src/components/ui/Landingpage/Steps.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  number: number
+  title: string
+  description: string
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-4">
+    <div
+      class="select-none steps relative flex items-center justify-center w-[50px] h-[50px] border-2 border-[var(--primary-color)] bg-[var(--surface-color)] rounded-full font-bold text-[var(--primary-color)] text-2xl"
+    >
+      <span>{{ props.number }}</span>
+    </div>
+
+    <div class="flex flex-col items-center justify-center">
+      <span class="font-bold w-full text-center text-lg">{{ props.title }}</span>
+      <span class="text-center w-full text-[var(--text-color-light)] mt-2 max-w-[350px]"
+        >{{ props.description }}</span
+      >
+    </div>
+
+    <div
+      class="select-none w-full h-[180px] bg-[var(--background-color)] border border-[var(--border-color)] shadow-sm rounded-xl px-4 py-4"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
