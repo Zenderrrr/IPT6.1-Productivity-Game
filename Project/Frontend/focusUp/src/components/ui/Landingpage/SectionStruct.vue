18884c9e99efbaf29a553b3ae89d8a206a4ed196
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-07-14 23:19:42 +0200

    Update SectionStruct add show up animation

--- Project/Frontend/focusUp/src/components/ui/Landingpage/SectionStruct.vue
+++ Project/Frontend/focusUp/src/components/ui/Landingpage/SectionStruct.vue
@@ -1,21 +1,52 @@
 <script lang="ts" setup>
+import { onMounted, onUnmounted, ref } from 'vue'
+
 const props = defineProps<{
   kicker: string
   title: string
   subtitle: string
 }>()
+
+const sectionRef = ref<HTMLElement | null>(null)
+const isVisible = ref(false)
+
+let observer: IntersectionObserver
+
+onMounted(() => {
+  observer = new IntersectionObserver(
+    ([entry]) => {
+      if (entry.isIntersecting) {
+        isVisible.value = true
+        observer.unobserve(entry.target)
+      }
+    },
+    {
+      threshold: 0.25,
+    },
+  )
+
+  if (sectionRef.value) {
+    observer.observe(sectionRef.value)
+  }
+})
+
+onUnmounted(() => {
+  observer?.disconnect()
+})
 </script>
 
 <template>
-  <section class="px-8 py-30">
+  <section ref="sectionRef" class="px-8 py-30">
     <div class="max-w-[1400px] mx-auto">
       <div class="text-center">
-        <span class="uppercase text-[var(--primary-color)] text-sm font-semibold">{{
-            props.kicker
-          }}</span>
-        <h2 class="text-[40px] font-extrabold">{{ props.title }}</h2>
+        <span
+          :class="!isVisible ? 'opacity-0 -translate-y-[35px]' : 'opacity-100 translate-0'"
+          class="transition-all duration-700 uppercase text-[var(--primary-color)] text-sm font-semibold"
+          >{{ props.kicker }}</span
+        >
+        <h2 :class="!isVisible ? 'opacity-0 -translate-y-[35px]' : 'opacity-100 translate-0'" class="delay-100 transition-all duration-700 text-[40px] font-extrabold">{{ props.title }}</h2>
         <div class="w-full flex justify-center">
-          <p class="text-[var(--text-color-light)] text-md max-w-[600px]">
+          <p :class="!isVisible ? 'opacity-0 -translate-y-[35px]' : 'opacity-100 translate-0'" class="delay-200 transition-all duration-700 text-[var(--text-color-light)] text-md max-w-[600px]">
             {{ props.subtitle }}
           </p>
         </div>
