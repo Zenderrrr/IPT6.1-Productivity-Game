b45a86eea4edad67027046a8725a4acbeae4234e
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-08-26 21:48:25 +0200

    Create Loading.vue component

---
+++ Project/Frontend/focusUp/src/components/ui/Loading.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-[100vh] w-full max-w-[80rem] mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex flex-col justify-center items-center gap-3 text-[var(--text-color-light)] text-sm">
+      <div class="animate-spin w-12 h-12 bg-transparent rounded-full border-3 border-[var(--border-color)] border-t-[var(--primary-color)]">
+      </div>
+      <span>Wird geladen ...</span>
+    </div>
+  </div>
+</template>
